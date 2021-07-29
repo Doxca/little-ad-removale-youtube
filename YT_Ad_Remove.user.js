af29b7adc2bca38fe4765993531f88b3bc5a0775
Doxca <81829364+Doxca@users.noreply.github.com>
2021-07-29 17:02:58 +0200

    Update YT_Ad_Remove.user.js

--- YT_Ad_Remove.user.js
+++ YT_Ad_Remove.user.js
@@ -5,6 +5,10 @@
 // @author       Doxca
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
+// @namespace       https://github.com/Doxca/little-ad-removale-youtube
+// @updateURL       https://github.com/Doxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
+// @downloadURL     https://github.com/Doxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
+// @supportURL      https://github.com/Doxca/little-ad-removale-youtube/issues
 // @grant        none
 // ==/UserScript==
 
