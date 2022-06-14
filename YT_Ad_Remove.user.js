3c6dde789d091552923cc3425d2b0ed9a86bf3f4
Ender <81829364+Endxca@users.noreply.github.com>
2022-06-15 00:55:55 +0200

    Update YT_Ad_Remove.user.js

--- YT_Ad_Remove.user.js
+++ YT_Ad_Remove.user.js
@@ -2,13 +2,13 @@
 // @name         youtube ad remove
 // @version      0.1
 // @description  remove ads on main page (not in video :/)
-// @author       Doxca
+// @author       Ender
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
-// @namespace       https://github.com/Doxca/little-ad-removale-youtube
-// @updateURL       https://github.com/Doxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
-// @downloadURL     https://github.com/Doxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
-// @supportURL      https://github.com/Doxca/little-ad-removale-youtube/issues
+// @namespace       https://github.com/Endxca/little-ad-removale-youtube
+// @updateURL       https://github.com/Endxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
+// @downloadURL     https://github.com/Endxca/little-ad-removale-youtube/raw/main/YT_Ad_Remove.user.js
+// @supportURL      https://github.com/Endxca/little-ad-removale-youtube/issues
 // @grant        none
 // ==/UserScript==
 
